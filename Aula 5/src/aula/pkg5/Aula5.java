7a40e29bbe48a6300d8a2d22225ac273d33a3ab3
Abboud21 <abboudmucy@gmail.com>
2025-06-10 09:19:58 -0300

    Add files via upload

---
+++ Aula 5/src/aula/pkg5/Aula5.java
@@ -0,0 +1,19 @@
+
+package aula.pkg5;
+
+public class Aula5 {
+
+    public static void main(String[] args) {
+      
+        matematica mat=new matematica();
+        
+        mat.calculo(7);
+       
+        System.out.println(mat.calculo(7));
+        
+        mat.calculo(8,-44,-67);
+       
+        
+    }
+    
+}
