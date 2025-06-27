30c56309cee504418af37e7fa5dd22c3f825fdfa
Abboud <abboudmucy@gmail.com>
2025-06-27 09:31:57 -0300

    Add files via upload

---
+++ Swing_Matematica/src/swing_matematica/Matematica.java
@@ -0,0 +1,10 @@
+
+package swing_matematica;
+
+public class Matematica {
+    
+    public double soma(double a, double b){
+        return a+b;
+    }
+    
+}
---
+++ Swing_Matematica/src/swing_matematica/Swing_Matematica.java
@@ -0,0 +1,16 @@
+
+package swing_matematica;
+
+public class Swing_Matematica {
+
+    public static void main(String[] args) {
+       
+        Matematica matematica = new Matematica();
+        
+        double a = matematica.soma(5,6);
+        
+       
+        System.out.println(a);
+    }
+    
+}
