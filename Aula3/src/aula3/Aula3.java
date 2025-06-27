e6bc2258775e0c987b0938c69d2cc2133c41d15c
Abboud <abboudmucy@gmail.com>
2025-06-27 09:19:07 -0300

    Add files via upload

---
+++ Aula3/src/aula3/Aula3.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Aula3 {
+
+   static void delta(double a,double b,double c) {
+     Scanner tec =new Scanner(System.in);
+      
+      System.out.println("Escreva o valor de (a):: ");
+      a=tec.nextDouble();
+      System.out.println("Escreva o valor de (b):: ");
+      b=tec.nextDouble();
+      System.out.println("Escreva o valor de (c):: ");
+      c=tec.nextDouble();
+      
+      double resltD1=(b*b)-(4*a*c);
+         
+         
+         
+     }
+     static void bhaskara(double a,double b,double c) {
+     
+         
+         
+     }
+     
+     public static void main(String[] args) {
+         
+       
+         
+         
+         
+     }
+    
+}
